61cc691ef0aba9724032006cd753ede4985d51f8
Marcel Benders <git@b-velop.com>
2018-05-10 14:38:18 +0200

    Edit Server urls

--- Home.DataCrawler/Program.cs
+++ Home.DataCrawler/Program.cs
@@ -19,6 +19,7 @@ namespace Home.DataCrawler
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+		           .UseUrls("http://*:5001")
+		           .UseStartup<Startup>();
     }
 }
